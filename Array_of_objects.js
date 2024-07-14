9560d62128b1bd06b7d7ec9359b68d007a2c316d
Akshay Kalson <akshaykalson@gmail.com>
2024-07-14 13:42:13 +0200

    Add files via upload

---
+++ Array_of_objects.js
@@ -0,0 +1,22 @@
+//this is an array of objects, we can use them to store information
+
+var myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "year": 1995
+    },
+    {
+        "artist": "Michael Breus",
+        "title": "Cinema Man",
+        "year": 1997
+    }
+];
+
+
+// 2 ways to access the array
+
+console.log(myMusic[0]["title"])
+console.log(myMusic[0].title)
+
+
