1408f6e3eb7529ea031a41b8eda0e88f67a1f7b7
Akshay Kalson <akshaykalson@gmail.com>
2024-08-26 21:21:41 +0200

    Add files via upload

---
+++ loops_in_js.js
@@ -0,0 +1,14 @@
+var myArray = []
+
+var i = 10
+
+do{
+    myArray.push(i)
+    i++
+
+
+} while (i<5)
+//the do while loop will do the task atleast one time before checking the condition, then it will check condition
+
+console.log(myArray)
+
